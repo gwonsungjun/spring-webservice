7731f7e73b9eacf1c0dbf92c64e96d9d1b2728d3
권성준 <sungjunpizz@gmail.com>
2018-03-07 22:37:28 +0900

    3장, 3-1 메인 페이지 테스트 코드 작성 까지 완료

--- src/test/java/com/sungjunpizz/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/sungjunpizz/webservice/domain/posts/PostsRepositoryTest.java
@@ -9,10 +9,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import sun.util.resources.LocaleData;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -44,5 +47,24 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle(), is("테스트 게시글"));
         assertThat(posts.getContent(), is("테스트 본문"));
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                    .title("테스트 게시글")
+                    .content("테스트 본문")
+                    .author("kwen5600@naver.com")
+                    .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreateDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
     }
 }
