247fbcaa145b1b5a5ba184b0e7dffcab652c3c6e
권성준 <sungjunpizz@gmail.com>
2018-03-10 10:27:02 +0900

    3장 완료. 게시글 목록 확인

--- src/main/java/com/sungjunpizz/webservice/domain/posts/PostsRepository.java
+++ src/main/java/com/sungjunpizz/webservice/domain/posts/PostsRepository.java
@@ -1,6 +1,15 @@
 package com.sungjunpizz.webservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.stream.Stream;
 
 public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+    @Query("SELECT p "+
+            "From  Posts p " +
+            "ORDER BY p.id DESC")
+    Stream<Posts> findAllDesc();
+
 }
--- src/main/java/com/sungjunpizz/webservice/service/PostsService.java
+++ src/main/java/com/sungjunpizz/webservice/service/PostsService.java
@@ -2,9 +2,13 @@ package com.sungjunpizz.webservice.service;
 
 import com.sungjunpizz.webservice.domain.posts.PostsRepository;
 import com.sungjunpizz.webservice.dto.posts.PostSaveRequestDto;
+import com.sungjunpizz.webservice.dto.posts.PostsMainResponseDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -14,5 +18,12 @@ public class PostsService {
     @Transactional
     public Long save(PostSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
